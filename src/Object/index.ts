1ae2374212416644ad32dd11c82cb6afaaf75aed
biswass101 <biswassnaeemcse@gmail.com>
2025-08-06 22:25:33 +0600

    chore(interface): add interface

--- src/Object/index.ts
+++ src/Object/index.ts
@@ -1,20 +1,28 @@
-function printUser (user: {
+type User = {
     name: string;
     age: number;
     courses: string[];
-}) {
+}
+
+interface IUser {
+    name: string;
+    age: number;
+    courses: string[];
+}
+
+function printUser (user: User) {
     console.log(user.age);
     console.log(user.name);
     console.log(user.courses.join(', '));
 }
 
-const user1 = {
+const user1: IUser = {
     name: 'Jan',
     age: 27,
     courses: ['C', 'C++', 'Python', 'TypeScript'],
 };
 
-const user2 = {
+const user2: User = {
     name: 'Daniyel',
     age: 24,
     courses: ['Java', 'C#'],
